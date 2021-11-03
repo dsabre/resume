80a3082d5f8891d84fcebad9934595b579cdb082
Daniele Sabre <daniele.sabre@jakala.com>
2021-11-03 16:56:33 +0100

    Develop

--- deploy.js
+++ deploy.js
@@ -1,43 +1,47 @@
-const {execSync} = require('child_process');
-const chalk = require('chalk');
-const FtpDeploy = require("ftp-deploy");
-const ftpDeploy = new FtpDeploy();
-const config = {
-    user: "dsstuff",
-    password:   "VJh9w2sDKH82",
-    host:       "dsstuff.altervista.org",
-    port:       21,
-    localRoot:  __dirname + "/dist",
-    remoteRoot: "/resume/",
-    include: ["*", "**/*"],      // this would upload everything except dot files
-    // include: ["*.php", "dist/*", ".*"],
-    // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
-    // exclude: ["dist/**/*.map", "node_modules/**", "node_modules/**/.*", ".git/**"],
-    // delete ALL existing files at destination before uploading, if true
-    deleteRemote: false,
-    // Passive mode is forced (EPSV command is not sent)
-    forcePasv: true,
-    // use sftp or ftp
-    sftp: false
+const {execSync} = require("child_process");
+const fs         = require("fs");
+const chalk      = require("chalk");
+
+// github branch pages name
+const PAGES_BRANCH_NAME = 'gh-pages';
+
+const execSyncVerbose = command => {
+    console.log(command);
+    execSync(command);
 };
 
 try {
-    console.log(chalk.cyan('INFO: init build'));
-    execSync('npm run build');
-    console.log(chalk.cyan('INFO: end build'));
+    // get current branch name
+    let currentBranch = execSync("git branch |grep \\* |sed 's/\\* //g'");
+    currentBranch     = currentBranch.toString().trim();
 
-    console.log(chalk.cyan('INFO: init deploy'));
-    ftpDeploy
-        .deploy(config)
-        .then(() => {
-            console.log(chalk.cyan('INFO: end deploy'));
+    console.log(chalk.cyan('Deploy started from branch') + ' ' + chalk.bgBlue(chalk.yellowBright(currentBranch)));
 
-            execSync('rm -rf dist');
+    console.log('\n' + chalk.cyan('Committing uncommitted changes'));
+    execSyncVerbose('git add .');
+    execSyncVerbose('git commit -m "Develop"');
+    execSyncVerbose('git pull');
+    execSyncVerbose('git push');
 
-            console.log(chalk.green('SUCCESS: see on http://' + config.host + config.remoteRoot));
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    console.log('\n' + chalk.cyan('Build project'));
+    execSyncVerbose(`git checkout --orphan ${PAGES_BRANCH_NAME}`);
+    execSyncVerbose('npm run build');
+
+    const folderName = fs.existsSync('dist') ? 'dist' : 'build';
+    console.log('\n' + chalk.cyan(`Commit ${folderName} directory`));
+    execSyncVerbose(`git --work-tree ${folderName} add --all`);
+    execSyncVerbose(`git --work-tree ${folderName} commit -m "GitHub pages deploy"`);
+
+    console.log('\n' + chalk.cyan(`Pushing to ${PAGES_BRANCH_NAME}`));
+    execSyncVerbose(`git push origin HEAD:${PAGES_BRANCH_NAME} --force`);
+
+    console.log('\n' + chalk.cyan(`Return to ${currentBranch} branch`));
+    execSyncVerbose(`rm -rf ${folderName}`);
+    execSyncVerbose(`git checkout -f ${currentBranch}`);
+    execSyncVerbose(`git branch -D ${PAGES_BRANCH_NAME}`);
+
+    console.log('\n' + chalk.green('SUCCESSFULLY DEPLOYED'));
 } catch (err) {
+    console.log(chalk.red(err.message));
+    process.exit(1);
 }
