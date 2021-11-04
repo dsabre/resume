2cacd0368471895bd4f1c722937214004b8b50d3
Daniele Sabre <daniele.sabre@jakala.com>
2021-11-04 14:25:21 +0100

    Improved cookies

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,14 @@ import i18n    from '../i18n';
 
 Vue.use(Vuex);
 
+// cookie names
+const COOKIE_THEME  = 'theme';
+const COOKIE_LOCALE = 'locale';
+
 export default new Vuex.Store({
     state:     {
-        theme:       Cookies.get('theme') || 'light',
-        locale:      Cookies.get('locale') || i18n.fallbackLocale,
+        theme:       Cookies.get(COOKIE_THEME) || 'light',
+        locale:      Cookies.get(COOKIE_LOCALE) || i18n.fallbackLocale,
         sidebarOpen: false
     },
     mutations: {
@@ -16,13 +20,13 @@ export default new Vuex.Store({
             this.state.theme = theme;
 
             // save theme in cookie
-            Cookies.set('theme', theme, {expires: 7, path: '', secure: true, sameSite: 'strict'});
+            Cookies.set(COOKIE_THEME, theme, {expires: 7, path: '', secure: true, sameSite: 'strict'});
         },
         changeLocale(state, locale) {
             this.state.locale = locale;
 
             // save locale in cookie
-            Cookies.set('locale', locale, {expires: 7, path: '', secure: true, sameSite: 'strict'});
+            Cookies.set(COOKIE_LOCALE, locale, {expires: 7, path: '', secure: true, sameSite: 'strict'});
         }
     },
     actions:   {
