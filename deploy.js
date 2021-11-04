f44f71b90ce54f4db99a74ee30b43ffa6ea67db6
Daniele Sabre <daniele.sabre@jakala.com>
2021-11-04 13:15:04 +0100

    Fix deploy

--- deploy.js
+++ deploy.js
@@ -10,11 +10,25 @@ const execSyncVerbose = command => {
     execSync(command);
 };
 
-try {
-    // get current branch name
-    let currentBranch = execSync("git branch |grep \\* |sed 's/\\* //g'");
-    currentBranch     = currentBranch.toString().trim();
+// get current branch name
+let currentBranch = execSync("git branch |grep \\* |sed 's/\\* //g'");
+currentBranch     = currentBranch.toString().trim();
 
+const endProcess = (folderName) => {
+    console.log('\n' + chalk.cyan(`Return to ${currentBranch} branch`));
+
+    if (folderName) {
+        execSyncVerbose(`rm -rf ${folderName}`);
+    } else {
+        execSyncVerbose('rm -rf dist');
+        execSyncVerbose('rm -rf build');
+    }
+
+    execSyncVerbose(`git checkout -f ${currentBranch}`);
+    execSyncVerbose(`git branch -D ${PAGES_BRANCH_NAME}`);
+};
+
+try {
     console.log(chalk.cyan('Deploy started from branch') + ' ' + chalk.bgBlue(chalk.yellowBright(currentBranch)));
 
     try {
@@ -39,13 +53,13 @@ try {
     console.log('\n' + chalk.cyan(`Pushing to ${PAGES_BRANCH_NAME}`));
     execSyncVerbose(`git push origin HEAD:${PAGES_BRANCH_NAME} --force`);
 
-    console.log('\n' + chalk.cyan(`Return to ${currentBranch} branch`));
-    execSyncVerbose(`rm -rf ${folderName}`);
-    execSyncVerbose(`git checkout -f ${currentBranch}`);
-    execSyncVerbose(`git branch -D ${PAGES_BRANCH_NAME}`);
+    endProcess(folderName);
 
     console.log('\n' + chalk.green('SUCCESSFULLY DEPLOYED'));
 } catch (err) {
     console.error(chalk.red(err.message));
+
+    endProcess(false);
+
     process.exit(1);
 }
