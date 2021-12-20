dc10c03d042523da667413e7f1301675fcaa9058
Daniele Sabre <daniele.sabre@gmail.com>
2021-12-20 14:03:50 +0100

    Added Sentry

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
-import Vue from 'vue';
-import App from './App.vue';
-import i18n from './i18n';
+import Vue              from 'vue';
+import App              from './App.vue';
+import i18n             from './i18n';
 import './assets/css/tailwind.css';
-import store from './store';
+import store            from './store';
+import * as Sentry      from "@sentry/vue";
+import {Integrations}   from "@sentry/tracing";
+import * as packageJson from "../package.json";
 
 Vue.config.productionTip = false;
 
+Sentry.init({
+	Vue,
+	dsn:              "https://2bca6dc2f4d649bf9beb960566e16c0d@o312959.ingest.sentry.io/6114724",
+	integrations:     [
+		new Integrations.BrowserTracing({
+			tracingOrigins: ["localhost", "dsabre.github.io", /^\//],
+		}),
+	],
+	tracesSampleRate: process.env.NODE_ENV === 'development' ? 1.0 : 0.2,
+	environment:      process.env.NODE_ENV,
+	release:          packageJson.name + '@' + packageJson.version
+});
+
 new Vue({
-  i18n,
-  store,
-  render: h => h(App)
+	i18n,
+	store,
+	render: h => h(App)
 }).$mount('#app');
